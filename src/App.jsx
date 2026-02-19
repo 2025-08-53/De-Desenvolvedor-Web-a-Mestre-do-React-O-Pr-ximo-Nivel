322882906a0c5373c0dc3cae8ddef33e6348c3e6
Hudson Neves e Silva <Hudson50844637@edu.df.senac.br>
2026-02-19 17:04:23 -0300

    Aula 3 - Seção Sobre com layout em duas colunas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./sections/Hero";
 import Areas from "./sections/Areas";
+import Sobre from "./sections/Sobre";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Areas />
+      <Sobre />
     </>
   );
 }
