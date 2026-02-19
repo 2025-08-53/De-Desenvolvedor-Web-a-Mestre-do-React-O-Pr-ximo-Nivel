babbedaa8cddbb06331a8e03025a57e24118a0a2
Hudson Neves e Silva <Hudson50844637@edu.df.senac.br>
2026-02-19 16:58:16 -0300

    Aula 2 - Seção Áreas de Atuação com componente reutilizável

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./sections/Hero";
+import Areas from "./sections/Areas";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Areas />
     </>
   );
 }
