74cb4b2fd0ecb12eb0933e2f3ce7011b5dff08f3
HudsonNeves <hudsonnsilva@gmail.com>
2026-03-03 10:34:11 -0300

    feat: adiciona footer institucional na landing advocacia

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Hero from "./sections/Hero";
 import Areas from "./sections/Areas";
 import Sobre from "./sections/Sobre";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
@@ -10,6 +10,7 @@ function App() {
       <Hero />
       <Areas />
       <Sobre />
+      <Footer />
     </>
   );
 }
